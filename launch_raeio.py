6034a41687a85268b7ef9dde41672cc30852c44e
squaresyntax <35795783+squaresyntax@users.noreply.github.com>
2025-08-06 00:24:32 -0400

    Use Streamlit CLI directly and package data

--- launch_raeio.py
+++ launch_raeio.py
@@ -1,2 +1,10 @@
-import os
-os.system("streamlit run ui.py")
+import streamlit.web.cli as stcli
+
+
+def main() -> None:
+    """Launch the Streamlit UI."""
+    stcli.main(["run", "ui.py"])
+
+
+if __name__ == "__main__":
+    main()
